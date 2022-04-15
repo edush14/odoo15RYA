57da84a3a86bdb8886f87810fa7cf029625ef26e
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-04-15 18:34:40 -0500

    oka

--- rya_mrp_dev_js_it/models/mrp_production.py
+++ rya_mrp_dev_js_it/models/mrp_production.py
@@ -3,8 +3,9 @@ from odoo import fields, models , api
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
-
+    '''
     @api.onchange('qty_producing', 'lot_producing_id')
     def _onchange_producing(self):
         if self.state not in ['confirmed','progress','to_close']:
             self._set_qty_producing()
+    '''

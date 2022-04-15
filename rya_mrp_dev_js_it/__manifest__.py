0f910d57bbc7160b620b90e66e013c42449c3993
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-04-15 18:23:55 -0500

    oka

---
+++ rya_mrp_dev_js_it/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'NOmbre',
+    'category': 'uncategorize',
+    'author': 'ITGRUPO',
+    'depends': ['base'],
+    'version': '1.0',
+    'description':"""
+     Descripcion
+    """,
+    'auto_install': False,
+    'demo': [],
+    'data': [
+        #'security/security.xml',
+        #'views/views.xml',
+        ],
+    'installable': True
+}
---
+++ rya_mrp_dev_js_it/models/mrp_production.py
@@ -0,0 +1,10 @@
+from odoo import fields, models , api
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    @api.onchange('qty_producing', 'lot_producing_id')
+    def _onchange_producing(self):
+        if self.state not in ['confirmed','progress','to_close']:
+            self._set_qty_producing()

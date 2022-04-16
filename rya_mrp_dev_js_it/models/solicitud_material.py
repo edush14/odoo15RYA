624be4f9994e1b19c857c5be89faff4b1045e9c1
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-04-15 21:04:27 -0500

    oka

--- rya_mrp_dev_js_it/models/solicitud_material.py
+++ rya_mrp_dev_js_it/models/solicitud_material.py
@@ -36,13 +36,13 @@ class SolicitudProduction(models.Model):
             raise ValueError('No tiene Lineas de Productos')
         self.state = 'comfirm'
         for l in self.order_line:
-            raise ValueError(l.product_id.location_id.id)
+            raise ValueError(l.product_id.production_location_id.id)
             self.mrp_production.move_raw_ids += self.env['stock.move'].new({
                 'product_id': l.product_id.id ,
                 'name':  l.product_id.display_name,
                 'quantity_done': l.consumed,
                 'product_uom': l.product_id.uom_id.id,
-                'location_id': 	l.product_id.location_id.id,
+                'location_id': 	l.product_id.production_location_id.id,
 
             })
 

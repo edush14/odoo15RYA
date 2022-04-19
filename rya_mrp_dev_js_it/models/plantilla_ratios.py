4bda22a9ef2c4255a0738fa9dcd8fb194f72b013
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-04-19 14:49:22 -0500

    oka

--- rya_mrp_dev_js_it/models/plantilla_ratios.py
+++ rya_mrp_dev_js_it/models/plantilla_ratios.py
@@ -2,7 +2,7 @@ from odoo import fields, models , api , _
 class PlantillaRatios(models.Model):
     _name = 'plantilla.ratios'
     name  = fields.Char()
-    is_default = fields = fields.Boolean()
+    is_default = fields.Boolean()
     order_line = fields.One2many('plantilla.ratios.line', 'order_id', string='Order Lines', copy=True)
 
 class PlantillaRatiosLine(models.Model):

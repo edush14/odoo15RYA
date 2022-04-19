25d4acfc67838fc1c2e9cab3821429e7da5f7197
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-04-19 15:11:57 -0500

    oka

--- rya_mrp_dev_js_it/models/plantilla_ratios.py
+++ rya_mrp_dev_js_it/models/plantilla_ratios.py
@@ -1,12 +1,38 @@
-from odoo import fields, models , api , _
+from odoo import fields, models, api, _
+
+
 class PlantillaRatios(models.Model):
     _name = 'plantilla.ratios'
-    name  = fields.Char()
+    name = fields.Char(required=True)
     is_default = fields.Boolean()
     order_line = fields.One2many('plantilla.ratios.line', 'order_id', string='Order Lines', copy=True)
 
+    @api.model
+    def create(self, values):
+        res = super(PlantillaRatios, self).create(values)
+        # here you can do according
+        if not res.order_line:
+            names_array = [
+                'MOD',
+                'MOI - Operativo',
+                'MOI - Production',
+                'MOI - Control',
+                'ALQUILER',
+                'DEPRECIACION',
+                'MANTENIMIENTO',
+                'COMBUSTIBLE - FAMILIA 2',
+                'ENERGIA ELECTRICA'
+            ]
+            for n in names_array:
+                res.order_line += self.env['plantilla.ratios.line'].new({
+                    'name': n
+                })
+
+        return res
+
+
 class PlantillaRatiosLine(models.Model):
     _name = 'plantilla.ratios.line'
-    name = fields.Char()
+    name = fields.Char(required=True)
     price_unit = fields.Float(string="Costo Unitario")
     order_id = fields.Many2one('plantilla.ratios', ondelete='restrict')

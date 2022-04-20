ca16e218b4cbd4751c2f2f36d36424ef165ca05e
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-04-20 01:00:37 -0500

    oka

--- rya_mrp_dev_js_it/models/stock_move.py
+++ rya_mrp_dev_js_it/models/stock_move.py
@@ -7,7 +7,7 @@ class StockMove(models.Model):
     stage_id = fields.Many2one('stage.mrpline', string="Etapa")
     should_consume_qty_store = fields.Float('Quantity To Consume Store',
                                       digits='Product Unit of Measure')
-    @api.onchange('should_consume_qty')
+    @api.onchange('should_consume_qty','raw_material_production_id.qty_producing')
     def change_store(self):
         for record in self:
             record.should_consume_qty_store = record.should_consume_qty
